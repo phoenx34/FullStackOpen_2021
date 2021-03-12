f9799912759777c0365657fc4cf425132f4b25f6
phoenx34 <misaac34@gmail.com>
2021-03-12 13:49:38 -0500

    exercise 1.6 first commit

---
+++ part_1/unicafe/src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const Title = ({title}) => <div><h1>{title}</h1></div>
+
+const StatsWrapper = ({clicks}) => {
+  if(clicks.all === 0) return (<div>No feedback given.</div>)
+  return (
+    <div>
+      <table>
+        <tbody>
+          <Statistics text='good' value={clicks.good} />
+          <Statistics text='neutral' value={clicks.neutral} />
+          <Statistics text='bad' value={clicks.bad} />
+          <Statistics text='all' value={clicks.all} />
+          <Statistics text='average' value={(clicks.all) / 3} />
+          <Statistics text='positive' value={(clicks.good) / (clicks.all)} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const Statistics = ({text, value}) => {
+  return (
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+  )
+} 
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    all: 0
+  })
+
+  const feedback = 'give feedback'
+  const statistics = 'statistics'
+
+  const handleGoodClick = () => 
+    setClicks({...clicks, good: clicks.good + 1, all: clicks.all + 1})
+  const handleNeutralClick = () => 
+    setClicks({...clicks, neutral: clicks.neutral + 1, all: clicks.all + 1})
+  const handleBadClick = () => 
+    setClicks({...clicks, bad: clicks.bad + 1, all: clicks.all + 1})
+    
+
+
+  return(
+    <div>
+      <Title title={feedback} />
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Title title={statistics} />
+      <StatsWrapper clicks={clicks} />
+    </div>
+  )
+}
+
+export default App
